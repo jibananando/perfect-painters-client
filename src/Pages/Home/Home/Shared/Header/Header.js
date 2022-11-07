7ab26d0d3b0ecc9199f9824fdd547ad5ec42b854
jibananando <jibananando.pramanik@gmail.com>
2022-11-08 04:09:25 +0600

    added a navbar

--- src/Pages/Home/Home/Shared/Header/Header.js
+++ src/Pages/Home/Home/Shared/Header/Header.js
@@ -2,8 +2,28 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <h3>This is Header</h3>
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                {/* <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">Blog</a></li>
+                    </ul>
+                </div> */}
+                <a href="/" className="btn btn-ghost normal-case text-xl">Perfect Painters</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Blog</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a href="/" className="btn">Get started</a>
+            </div>
         </div>
     );
 };
